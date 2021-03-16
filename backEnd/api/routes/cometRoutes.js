bb970ec3d48bb182628d1168aba9bfd85294db64
Ethan Kerdelhue <ethan.kerdelhue@epitech.eu>
2021-03-16 17:19:17 +0100

    [Update] Edit .gitignore, add middleware directory and authMiddleware.js

--- backEnd/api/routes/cometRoutes.js
+++ backEnd/api/routes/cometRoutes.js
@@ -1,15 +1,16 @@
 'use strict';
 module.exports = function(app) {
   var comet = require('../controllers/CometController');
-
+  var authenticateToken = require('../middleware/authMiddleware')
+  
   // user Routes
   app.route('/comet')
-    .get(comet.list_all_comet)
-    .post(comet.create_a_comet);
+    .get(authenticateToken, comet.list_all_comet, )
+    .post(authenticateToken, comet.create_a_comet, );
 
 
   app.route('/comet/:cometId')
-    .get(comet.read_a_comet)
-    .put(comet.update_a_comet)
-    .delete(comet.delete_a_comet);
+    .get(authenticateToken, comet.read_a_comet, authenticateToken)
+    .put(authenticateToken, comet.update_a_comet)
+    .delete(authenticateToken, comet.delete_a_comet);
 };
--- backEnd/api/routes/meetupsRoutes.js
+++ backEnd/api/routes/meetupsRoutes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   var meetup = require('../controllers/meetupsController');
 
   // meetup Routes
-  app.route('/meetups')
+  app.route('/meetup')
     .get(meetup.list_all_meetups)
     .post(meetup.create_a_meetup);
 
@@ -13,3 +13,4 @@ module.exports = function(app) {
     .put(meetup.update_a_meetup)
     .delete(meetup.delete_a_meetup);
 };
+
