3c6a6f0bfe97d957dcdaa6a413b8454558795a32
Ethan Kerdelhue <ethan.kerdelhue@epitech.eu>
2021-03-16 17:28:43 +0100

    [Update] Prevent directory name repetition

---
+++ api/routes/cometRoutes.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(app) {
+  var comet = require('../controllers/CometController');
+  var authenticateToken = require('../middleware/auth')
+  
+  // user Routes
+  app.route('/comet')
+    .get(authenticateToken, comet.list_all_comet)
+    .post(authenticateToken, comet.create_a_comet);
+
+
+  app.route('/comet/:cometId')
+    .get(authenticateToken, comet.read_a_comet)
+    .put(authenticateToken, comet.update_a_comet)
+    .delete(authenticateToken, comet.delete_a_comet);
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+  var express = require('express'),
+  cors = require('cors')
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  User = require('./api/models/userModel'), //created model loading here
+  Meetups = require('./api/models/meetupsModel'),
+  bodyParser = require('body-parser');
+  commet = require('./api/models/cometModels'),
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/mycometdb');
+
+/*  "/api/status"
+ *   GET: Get server status
+ *   PS: it's just an example, not mandatory
+ */
+app.get("/api/status", function (req, res) {
+    res.status(200).json({ status: "UP" });
+});
+
+function manageError(res, reason, message, code) {
+    console.log("Error: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+var routes = require('./api/routes/userRoutes');
+var meetups = require('./api/routes/meetupsRoutes');
+var auth = require('./api/routes/authRoutes');
+var comet = require('./api/routes/cometRoutes');
+
+routes(app); //register the route
+meetups(app);
+auth(app);
+comet(app);
+
+app.listen(port);
+
+
+console.log('my comet RESTful API server started on: ' + port);
